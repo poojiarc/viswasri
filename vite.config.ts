b7b97c8d6fb5c6f97e8dbeb7f21ddcff476be493
poojiarc <poojimedabayina@gmail.com>
2026-02-18 09:56:34 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,25 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true, // allow external access
     port: 8080,
+
+    allowedHosts: [
+      "timmy-semimature-unpoisonously.ngrok-free.dev",
+    ],
+
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
